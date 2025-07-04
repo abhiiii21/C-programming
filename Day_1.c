2613b3561b7e39b7ec5456b601587caebb6b58b5
Abhijeet <abhijeetthube215@gmail.com>
2025-07-04 23:47:33 +0530

    input output

---
+++ Day_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    float b;
+    char abhi;
+    printf("Enter character value\n");
+    scanf("%c",&abhi);
+    printf("Enter an integer value\n");
+    scanf("%d",&a);
+    printf("Enter a decimal value\n");
+    scanf("%f",&b);
+    printf("Entered character value is %c\n",abhi);
+    printf("Entered integer value is %d\n",a);
+    printf("Entered decimal value is %f",b);
+}

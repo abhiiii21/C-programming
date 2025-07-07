6cfefc2dea62f8698156be998971a3650e92c522
Abhijeet <abhijeetthube215@gmail.com>
2025-07-07 23:59:46 +0530

    operators

---
+++ operators.c
@@ -0,0 +1,40 @@
+
+// program 1 (logical operators)
+// #include<stdio.h>
+// void main()
+// {
+//   int a,b,result;
+//   a=5,b=10;
+//   result= a>b && b!=10 && b<11;
+//   printf("%d",result);  
+// }
+
+
+//program 2 (logical)
+// #include<stdio.h>
+// void main()
+// {
+//   int a,b,result;
+//   a=10,b=5;
+//   result= a<b || b==5;
+//   printf("%d",result);  
+// }
+
+
+//program 3 (logical)
+// #include<stdio.h>
+// void main()
+// {
+//   int a,b,result;
+//   a=4,b=6;
+//   result= !(a>b);
+//   printf("%d",result);    
+// }
+
+
+//program 4 (bitwise)
+#include<stdio.h>
+void main()
+{
+    
+}
